5ecf30ac967ccc30b70aa6d9f94a516be1ec8094
Daniel Barboni <serpoiu@gmail.com>
2025-11-29 20:45:24 +0000

    cleared review mentions and finished deploying the spa. it works and connects to the azure database

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { buildReviewUses, REVIEWS_KEY } from './config/appServices';
+import { getDeviceService, DEVICE_KEY } from './config/appServices';
 
 const app = createApp(App);
 
 app.use(router);
 
 // Provide bound review use cases to the app's DI container
-app.provide(REVIEWS_KEY, buildReviewUses());
+app.provide(DEVICE_KEY, getDeviceService());
 
 app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ListReviews from '@/views/ListReviews.vue';
+import ListDevices from '@/views/devices/ListDevices.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/devices',
+  },
+  {
+    path: '/devices',
+    name: 'Devices',
+    component: ListDevices,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: ListReviews,
-    },
-  ],
+  routes,
 });
 
 export default router;
